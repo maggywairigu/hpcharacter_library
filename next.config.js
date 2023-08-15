5a589d0a3aca3f324ad880ca5ba66187d11e0d13
maggywairigu <wairigumaina2016@gmail.com>
2023-08-15 10:26:55 +0300

    Home page, navbar complete. API successfully integrated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['ik.imagekit.io'], // Add the domain of the external images
+      },
+}
 
 module.exports = nextConfig
