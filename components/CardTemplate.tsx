4a8699e0ca252b5ed2afb9afe391c2c07a43ecb7
maggywairigu <wairigumaina2016@gmail.com>
2024-10-24 13:20:36 +0300

    home page bug fix

--- components/CardTemplate.tsx
+++ components/CardTemplate.tsx
@@ -17,7 +17,7 @@ const CardTemplate: React.FC<CharacterCardProps> = ({ image, name, dateOfBirth, 
   return (
     <motion.div
       variants={fadeIn('right', 'spring', 0.5, 0.05)}
-      className='w-full p-1 md:p-2 lg:p-3 xl:p-4 rounded-lg relative flex flex-col md:flex-row items-center shadow-md'
+      className='rounded-lg relative flex flex-col md:flex-row items-center shadow-md'
     >
       <Tilt
         options={{
@@ -25,21 +25,22 @@ const CardTemplate: React.FC<CharacterCardProps> = ({ image, name, dateOfBirth, 
           scale: 1,
           speed: 450,
         }}
-        className="bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 p-5 rounded-2xl md:w-[360px] w-full"
+        className="bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 p-4 rounded-2xl w-[250px]"
       >
         <div>
-          <Image src={image} width={300} height={150} alt={name} className='rounded m-auto' />
+          <Image src={image} width={150} height={50} alt={name} className='rounded m-auto' />
         </div>
-        <div className='mt-5 md:ml-5'>
-          <h1 className='p-4 text-primary font-bold text-xl md:text-2xl'>Name:</h1>
-          <h3 className='m-2 p-4 text-white text-sm md:text-base'>{name}</h3>
-          <h1 className='p-4 text-primary font-bold text-xl md:text-2xl mt-5'>Date of Birth:</h1>
-          <h3 className='m-2 p-4 text-white text-sm md:text-base'>{dateOfBirth}</h3>
+        <div className='mt-5 md:ml-5 text-center'>
+          <h1 className='text-primary font-bold text-sm'>Name:</h1>
+          <h3 className='mt-2 text-white text-sm'>{name}</h3>
+          <h1 className='mt-2 text-primary font-bold text-sm'>Date of Birth:</h1>
+          <h3 className='mt-2 mb-2 text-white text-sm'>{dateOfBirth}</h3>
         </div>
         <Link
           href={`/character/${encodeURIComponent(id)}`}
           
-          className='text-white font-bold black-gradient w-full md:w-[300px] h-10 md:h-12 rounded-md flex justify-center items-center cursor-pointer bg-primary hover:bg-tertiary mt-5 md:mt-0'
+          className='text-white font-bold black-gradient h-5 md:h-12 rounded-md flex
+          justify-center items-center cursor-pointer bg-primary hover:bg-tertiary mt-5 md:mt-0'
 
         >
           
